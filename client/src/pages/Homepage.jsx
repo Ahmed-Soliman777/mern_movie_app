fd4e1fb646c25fd4a119e7b0952307934502c45a
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-17 23:46:52 +0300

    add card list component to home page

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import Hero from '../components/Hero'
+import CardList from '../components/CardList'
 
 export default function Homepage() {
   return (
     <div className='p-5'>
       <Hero />
+      <CardList />
     </div>
   )
 }
