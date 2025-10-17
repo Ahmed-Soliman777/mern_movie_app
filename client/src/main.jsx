6abd75428a07e3ce48d7a0da862b2fb59bc4ab62
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-17 23:11:53 +0300

    add home page to main file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Hero from './components/Hero.jsx'
+import Homepage from './pages/Homepage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Hero />
+    <Homepage />
   </StrictMode>,
 )
