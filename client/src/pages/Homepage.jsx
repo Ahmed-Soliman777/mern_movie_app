4ce7cb3f787e59d21220bce86f47eb69a002bb06
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-18 00:22:53 +0300

    add footer component to home page

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,11 +1,15 @@
 import Hero from '../components/Hero'
 import CardList from '../components/CardList'
+import Footer from '../components/Footer'
 
 export default function Homepage() {
   return (
     <div className='p-5'>
       <Hero />
       <CardList />
+      <CardList />
+      <CardList />
+      <Footer />
     </div>
   )
 }
