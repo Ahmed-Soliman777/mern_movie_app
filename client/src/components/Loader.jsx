a34e2f35065977c515649d88b9570a97646df9fd
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-19 00:54:23 +0300

    edit class to className

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,7 +1,5 @@
 export default function Loader() {
     return (
-        <div className="flex items-center justify-center h-screen">
-            <span class="loader"></span>
-        </div>
+        <span className="loader"></span>
     )
 }
