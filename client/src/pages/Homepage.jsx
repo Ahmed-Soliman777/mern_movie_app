ffabe94cb2d6faf64199372c79f9ef7278ce8b42
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-18 15:44:25 +0300

    add react router to react

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -6,9 +6,10 @@ export default function Homepage() {
   return (
     <div className='p-5'>
       <Hero />
-      <CardList />
-      <CardList />
-      <CardList />
+      <CardList title={'Now Playing'} category={'now_playing'}/>
+      <CardList title={'Top Rated'} category={'top_rated'}/>
+      <CardList title={'Popular'} category={'popular'}/>
+      <CardList title={'Upcoming'} category={'upcoming'}/>
       <Footer />
     </div>
   )
