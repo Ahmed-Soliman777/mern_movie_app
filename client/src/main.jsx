0786456aaed63f40a26b962436afa0029205f70f
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-18 15:50:58 +0300

    add browser router to main file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Homepage from './pages/Homepage.jsx'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Homepage />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
