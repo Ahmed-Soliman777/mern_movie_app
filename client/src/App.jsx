a7dd1d16ccc06c28177363ed68ccb3e37c564de6
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-21 01:21:48 +0300

    add router to app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar'
 import Homepage from './pages/Homepage'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Moviepage from './pages/Moviepage'
+import Layout from './layout/Layout'
 
 export default function App() {
+
+  const routes = createBrowserRouter([
+    { path: "/", element: <Layout />, children:[
+      { path: "/", element: <Homepage /> },
+      { path: "/movie/:id", element: <Moviepage /> },
+    ] },
+  ])
+
   return (
     <>
-      <Navbar />
-      <Homepage />
+      <RouterProvider router={routes} />
     </>
   )
 }
