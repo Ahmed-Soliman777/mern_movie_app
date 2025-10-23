7ee411082154715959104d41b477542584ca7fa7
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-24 01:56:52 +0300

    add signin routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,18 @@ import Homepage from './pages/Homepage'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Moviepage from './pages/Moviepage'
 import Layout from './layout/Layout'
+import Signin from './pages/Signin'
 
 export default function App() {
 
   const routes = createBrowserRouter([
-    { path: "/", element: <Layout />, children:[
-      { path: "/", element: <Homepage /> },
-      { path: "/movie/:id", element: <Moviepage /> },
-    ] },
+    {
+      path: "/", element: <Layout />, children: [
+        { path: "/", element: <Homepage /> },
+        { path: "/movie/:id", element: <Moviepage /> },
+      ]
+    },
+    { path: "/signin", element: <Signin /> },
   ])
 
   return (
