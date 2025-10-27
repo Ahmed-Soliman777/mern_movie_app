7f223c4218542212b798659f7a2212a4337d54f5
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-28 02:00:41 +0300

    add user name when user is logged in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import { Search } from 'lucide-react';
 import logo from '../assets/logo.png'
 import { Link } from 'react-router';
+import { useAuthStore } from './../store/authStore';
 
 export default function Navbar() {
+  const { user } = useAuthStore()
+  
   return (
     <nav className='bg-black text-gray-200 flex justify-between items-center p-4 h-20 text-sm md:text-[15px] font-medium text-nowrap'>
       <Link to={"/"}>
@@ -23,9 +26,13 @@ export default function Navbar() {
           <Search className='absolute top-2 right-4 w-5 h-5' />
         </div>
         <button className='capitalize bg-[#e50914] px-5 py-2 text-white cursor-pointer'>get AI movie picks</button>
-        <Link to={'/signin'}>
+        {!user ? <Link to={'/signin'}>
           <button className='capitalize border border-[#333333] py-2 px-4 cursor-pointer'>sign in</button>
-        </Link>
+        </Link> :
+        
+        
+          <div className="text-white">{user.username}</div>
+        }
       </div>
     </nav>
   )
