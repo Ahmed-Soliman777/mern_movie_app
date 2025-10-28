74d875755c9b5f32799386032cd3d54812d10813
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-28 18:09:05 +0300

    add fetchUser property method to useAuthStore

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -39,5 +39,24 @@ export const useAuthStore = create((set) => ({
             set({ isLoading: false, error: error.response.data.message })
             throw error
         }
+    },
+
+    fetchUser: async () => {
+        set({ isLoading: true, error: null })
+        try {
+            const response = await axios.get(`http://localhost:3000/profile`)
+            set({ user: response.data.user, isLoading: false, fetchingUser: false })
+        } catch (error) {
+            if (
+                error.response &&
+                (error.response.status === 401 ||
+                    error.response.data.message === "No token provided!")
+            ) {
+                set({ user: null, fetchingUser: false, isLoading: false });
+                return null;
+            }
+            set({ isLoading: false, fetchingUser: false, error: error.response.data.message })
+            return null
+        }
     }
 }))
