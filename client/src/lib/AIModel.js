ae6d66ae1da6365c8bee73bd3d7f964044781337
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-29 01:14:10 +0300

    Add ai model config

---
+++ client/src/lib/AIModel.js
@@ -0,0 +1,26 @@
+import { GoogleGenAI } from '@google/genai'
+
+const ai = new GoogleGenAI({
+    api: import.meta.env.GOOGLE_GENAI_API_KEY
+})
+
+const config = {
+    responseMimeType: 'text/plain'
+}
+
+const model = 'gemini-2.0-flash'
+
+export async function getAIRecommendation(prompt) {
+    try {
+        const response = await ai.models.generateContent({
+            model,
+            config,
+            contents: [{ role: 'user', parts: [{ text: prompt }] }]
+        })
+
+        return response?.candidates?.[0]?.content?.parts?.[0]?.text
+    } catch (error) {
+        console.error(error);
+        return null
+    }
+}
